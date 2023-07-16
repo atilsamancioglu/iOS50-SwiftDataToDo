178b149672eda5bafb0c017960821481144eb201
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-07-16 14:11:18 +0300

    Initial Commit

---
+++ SwiftDataToDo/SwiftDataToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataToDoApp.swift
+//  SwiftDataToDo
+//
+//  Created by Atil Samancioglu on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
